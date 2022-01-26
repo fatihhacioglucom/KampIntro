409555a8d477a49cab0d0cd173121f90f7f74d44
Fatih Hacıoğlu <fatihhacioglucom@gmail.com>
2022-01-26 22:22:02 +0300

    5. Ders - Ödev 1 Recap

---
+++ Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Engin";
+            customer.LastName = "Demiroğ";
+            customer.City = "Ankara";
+
+            Customer customer2 = new Customer { Id = 2, FirstName = "Fatih", LastName = "Hacıoğlu", City = "Rize" };
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfacesIntro();
+
+            //Demo();
+
+            ICustomerDal[] customerDals = new ICustomerDal[] { new SqlServerCustomerDal(), new OracleCustomerDal(), new MySqlCustomerDal() };
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new OracleCustomerDal());
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager personManager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirsName = "Engin",
+                LastName = "Demiroğ",
+                Adress = "Ankara"
+            };
+
+            Student student = new Student();
+            student.Id = 1;
+            student.FirsName = "Derin";
+            student.LastName = "Demiroğ";
+            student.Departmant = "Computer Sciences";
+
+            personManager.Add(customer);
+            personManager.Add(student);
+        }
+    }
+
+    //Soyut Nesne
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirsName { get; set; }
+        string LastName { get; set; }
+    }
+
+    //Somut Nesne
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+
+        public string Adress { get; set; }
+    }
+
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+
+        public string Departmant { get; set; }
+    }
+
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+
+        public string Departmant { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirsName);
+        }
+    }
+}
