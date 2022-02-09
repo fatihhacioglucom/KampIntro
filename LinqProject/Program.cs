c749e763d802bcb83a79ad4d77ce54dbeb621460
Fatih Hacıoğlu <fatihhacioglucom@gmail.com>
2022-02-09 11:34:52 +0300

    7. Ders - Ödev 3

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -16,12 +16,81 @@ namespace LinqProject
             List<Product> products = new List<Product>
             {
                 new Product{ProductId=1,CategoryId=1,ProductName="Acer Laptop",QuantityPerUnit="32 GB Ram", UnitPrice=10000, UnitsInStock=5},
-                new Product{ProductId=1,CategoryId=1,ProductName="Asus Laptop",QuantityPerUnit="16 GB Ram", UnitPrice=8000, UnitsInStock=3},
-                new Product{ProductId=1,CategoryId=1,ProductName="Hp Laptop",QuantityPerUnit="8 GB Ram", UnitPrice=6000, UnitsInStock=2},
-                new Product{ProductId=1,CategoryId=2,ProductName="Samsung Telefon",QuantityPerUnit="4 GB Ram", UnitPrice=5000, UnitsInStock=15},
-                new Product{ProductId=1,CategoryId=2,ProductName="Apple Telefon",QuantityPerUnit="4 GB Ram", UnitPrice=8000, UnitsInStock=0}
+                new Product{ProductId=2,CategoryId=1,ProductName="Asus Laptop",QuantityPerUnit="16 GB Ram", UnitPrice=8000, UnitsInStock=3},
+                new Product{ProductId=3,CategoryId=1,ProductName="Hp Laptop",QuantityPerUnit="8 GB Ram", UnitPrice=6000, UnitsInStock=2},
+                new Product{ProductId=4,CategoryId=2,ProductName="Samsung Telefon",QuantityPerUnit="4 GB Ram", UnitPrice=5000, UnitsInStock=15},
+                new Product{ProductId=5,CategoryId=2,ProductName="Apple Telefon",QuantityPerUnit="4 GB Ram", UnitPrice=8000, UnitsInStock=0}
             };
 
+            //Test(products);
+
+            //GetProducts(products);
+
+            //AnyTest(products);
+
+            //FindTest(products);
+
+            //FindAllTest(products);
+
+            //AscDescTest(products);
+
+            //ClassicLinqTest(products);
+
+            var result = from p in products
+                         join c in categories
+                         on p.CategoryId equals c.CategoryId
+                         where p.UnitPrice > 5000
+                         orderby p.UnitPrice descending
+                         select new ProductDto { ProductId = p.ProductId, CategoryName = c.CategoryName, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+            
+            foreach (var productDto in result)
+            {
+                Console.WriteLine("{0} --- {1}", productDto.ProductName, productDto.CategoryName);
+            }
+        }
+
+        private static void ClassicLinqTest(List<Product> products)
+        {
+            var result = from p in products
+                         where p.UnitPrice > 6000
+                         orderby p.UnitPrice descending, p.ProductName ascending
+                         select new ProductDto { ProductId = p.ProductId, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void AscDescTest(List<Product> products)
+        {
+            var result = products.Where(p => p.ProductName.Contains("top")).OrderBy(p => p.UnitPrice).ThenByDescending(p => p.ProductName);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void FindAllTest(List<Product> products)
+        {
+            var result = products.FindAll(p => p.ProductName.Contains("top"));
+            Console.WriteLine(result);
+        }
+
+        private static void FindTest(List<Product> products)
+        {
+            var result = products.Find(p => p.ProductId == 3);
+            Console.WriteLine(result.ProductName);
+        }
+
+        private static void AnyTest(List<Product> products)
+        {
+            var result = products.Any(p => p.ProductName == "Acer Laptop");
+            Console.WriteLine(result);
+        }
+
+        private static void Test(List<Product> products)
+        {
             Console.WriteLine("Algoritmik-----------------");
 
             foreach (var product in products)
@@ -39,8 +108,6 @@ namespace LinqProject
             {
                 Console.WriteLine(product.ProductName);
             }
-
-            GetProducts(products);
         }
 
         static List<Product> GetProducts(List<Product> products)
@@ -60,6 +127,14 @@ namespace LinqProject
         {
             return products.Where(product => product.UnitPrice > 5000 && product.UnitsInStock > 3).ToList();
         }
+    }
+
+    class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
     }
 
     class Product
